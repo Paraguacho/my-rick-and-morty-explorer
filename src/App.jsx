5df53b989bbce26de370275e958bd6f011559c64
Paraguacho <sebastiansoq03@hotmail.com>
2026-01-30 15:17:40 -0700

    Funcionando

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from "react";
+import { Table, Typography } from "antd";
+import { getColumns } from "./columns";
+
+const {Title} = Typography;
+
+function App() {
+    //Estado de los personajes
+    const [characters, setCharacters] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [total, setTotal] = useState(0);
+    const [currentPage, setCurrentPage] = useState(1);
+
+    //Mostrar detalles
+    const handleViewDetail = (id) =>{
+        console.log(`Personaje id: ${id}`)
+    }
+
+    const fetchCharacters = async(page = 1) => {
+        setLoading(true)
+        try {
+            const response = await fetch(`https://rickandmortyapi.com/api/character?page=${page}`)
+            const data = await response.json()
+
+            //Guarda characters y el total para la pagina 
+            setCharacters(data.results);
+            setTotal(data.info.count);
+        } catch (error) {
+            console.log("Error mostrando personajes: " , error)
+        } finally{
+            setLoading(false)
+        };
+    };
+    
+    useEffect(()=>{
+        fetchCharacters();
+    }, []);
+
+    //Tabla para limpiar JSX
+    const tablePagination = {
+        current: currentPage,
+        total: total,
+        pageSize: 20,
+        onChange: (page)=>{
+            setCurrentPage(page)
+            fetchCharacters(page)
+        }
+    };
+
+    return(
+        <div>
+            <Title> Rick and Morty Explorer</Title>
+            <Table
+                columns={getColumns(handleViewDetail)}
+                dataSource={characters}
+                loading={loading}
+                rowKey="id"
+                pagination={tablePagination}
+            />
+        </div>
+    );
+}
+
+export default App;
