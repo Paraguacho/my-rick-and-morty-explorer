1c6ac162282bae9a10e10bdcc5c9b400275cc16e
Paraguacho <sebastiansoq03@hotmail.com>
2026-01-30 19:04:09 -0700

    Input funcionando

--- src/App.jsx
+++ src/App.jsx
@@ -13,31 +13,46 @@ function App() {
     const [loading, setLoading] = useState(false);
     const [total, setTotal] = useState(0);
     const [currentPage, setCurrentPage] = useState(1);
+    const [searchName, setSearchName] = useState('');
 
     //Mostrar detalles
     const handleViewDetail = (id) =>{
         console.log(`Personaje id: ${id}`)
     }
 
-    const fetchCharacters = async(page = 1) => {
+    const fetchCharacters = async(page = 1,name = '') => {
         setLoading(true)
         try {
-            const response = await fetch(`https://rickandmortyapi.com/api/character?page=${page}`)
+            const url = `https://rickandmortyapi.com/api/character?page=${page}&name=${name}`
+            const response = await fetch(url)
             const data = await response.json()
 
-            //Guarda characters y el total para la pagina 
-            setCharacters(data.results);
-            setTotal(data.info.count);
+            // Mostrar mensaje si no hay resultaods
+            if(response.ok && data.results){
+                setCharacters(data.results);
+                setTotal(data.info.count);
+            }else{
+                setCharacters([]);
+                setTotal(0)
+            }
         } catch (error) {
             console.log("Error mostrando personajes: " , error)
+            setCharacters([])
+            setTotal(0)
         } finally{
             setLoading(false)
         };
     };
     
     useEffect(()=>{
-        fetchCharacters();
-    }, []);
+        //Debounce para la busqueda tardia
+        const delayDebounce = setTimeout(() => {
+            fetchCharacters(1,searchName);
+            //Página nueva 
+            setCurrentPage(1)
+        }, 500);
+        return() => clearTimeout (delayDebounce)
+    }, [searchName]);
 
     //Tabla para limpiar JSX
     const tablePagination = {
@@ -70,8 +85,23 @@ function App() {
                
             }}>
                 Rick And Morty Explorer
-            </Title>  
+            </Title> 
+            
+            <div style={{
+                display:'flex',
+                justifyContent: 'center',
+                marginBottom: '40px'
+                }}>
+                <Input
+                    placeholder="Buscar..."
+                    allowClear
+                    style={{maxWidth:'500px'}}
+                    onChange={(est)=>{ setSearchName(est.target.value)}}   
+                />
+            </div> 
+            
             <Row gutter={[16,16]}>
+                {characters.length > 0} 
                     {characters.map((character)=>(
                         <Col key={character.id} style={{flex: '1 0 18%', maxWidth:'20%'}} xs={24} sm={12} md={8} lg ={6} xl = {6}>
                             <CharacterCard
@@ -94,7 +124,7 @@ function App() {
                     pageSize={20} 
                     onChange={(page) => {
                         setCurrentPage(page);
-                        fetchCharacters(page);
+                        fetchCharacters(page, searchName);
                     }}
                     showSizeChanger={false}
                 />
